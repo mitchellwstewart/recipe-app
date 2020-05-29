c36e9f4bb5e1051dc54914bb1f72a083204efbfa
Mitchell Stewart <mitchellstewart@Mitchells-MBP.home>
2020-05-29 18:39:50 -0400

    builds out authentication front end forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import AuthPage from './pages/Auth'
 import RecipesPage from './pages/Recipes'
 import SubscriptionPage from './pages/Subscription'
+import MainNavigation from './components/Navigation/MainNavigation'
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/recipes" component={RecipesPage} />
-        <Route path="/subscriptions" component={SubscriptionPage} />
-      </Switch>
-      
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/recipes" component={RecipesPage} />
+            <Route path="/subscriptions" component={SubscriptionPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
     
   );
