f6296d8001329423f560acbf65c55468cde1f5d5
Mitchell Stewart <mitchellwstewart@gmail.com>
2020-06-05 16:14:38 -0400

    creates grid view of recipes

--- frontend/src/components/Recipes/RecipeItem/RecipeItem.js
+++ frontend/src/components/Recipes/RecipeItem/RecipeItem.js
@@ -4,21 +4,19 @@ import './RecipeItem.scss'
 const recipeItem = props => {
   console.log(props)
   return (
-    <li key={props.recipeId} className="recipe__list-item f jcb">
-    <div>
-      <h1>{props.recipeName}</h1>
-      <h3>Estimated Time: {props.minutesEstimate} mins</h3>
-      <h3>Date Added: {new Date(props.date).toLocaleDateString()}</h3>
+    <li key={props.recipeId} className="recipe__list-item f jcb pointer" onClick={props.onDetail.bind(this, props.recipeId)}>
+    <div className="recipe__list-item_image">
+
+    </div>
+    <div className="recipe__list-item_container">
+      <h1 className="recipe__list-item_title">{props.recipeName}</h1>
+      <h3 className="recipe__list-item_time">{props.minutesEstimate} mins</h3>
+      <h3 >Date Added: {new Date(props.date).toLocaleDateString()}</h3>
     </div> 
     <div>
-      {props.userId === props.creatorId 
-      ? (
-        <React.Fragment>
-          <p>Your the owner of this recipe</p>
-          <button className="btn" onClick={props.onDetail.bind(this, props.recipeId)}>View Details</button>
-        </React.Fragment>
-      )
-      : (<button className="btn" onClick={props.onDetail.bind(this, props.recipeId)}>View Details</button>)
+      {props.userId === props.creator._id 
+      ? <p className="recipe__list-item_owner">Your the owner of this recipe</p>
+      : <p className="recipe__list-item_owner">Saved by {props.creator.email}</p>
       }
     </div>
   </li>
